1a6d98cf1915b1dfc930515eda782a8d54cebed5
tve24el024-blip <tve24el024@cet.ac.in>
2026-01-28 23:41:59 +0530

    committed

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,9 +1,35 @@
-public class Cupcake {
-    
-        // TODO: Read n
-        // TODO: Read calorie counts into an array
-        // TODO: Sort the array (think about ascending vs descending)
-        // TODO: Calculate the minimum miles using the formula (c * 2^j)
-        // TODO: Print the result (Use 'long' for the sum to avoid overflow)
-    
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Cupcake{
+
+    static long minimumMiles(int[] calories, int n) {
+        // Sort in ascending order
+        Arrays.sort(calories);
+
+        long miles = 0;
+        long power = 1; // represents 2^j
+
+        // Traverse from largest to smallest calorie
+        for (int i = n - 1; i >= 0; i--) {
+            miles += calories[i] * power;
+            power *= 2;
+        }
+
+        return miles;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] calories = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            calories[i] = sc.nextInt();
+        }
+
+        System.out.print(minimumMiles(calories, n));
+        sc.close();
+    }
 }
