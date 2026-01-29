854574044bd0a46ba935c35fed0418c2912cf4c6
tve24el024-blip <tve24el024@cet.ac.in>
2026-01-29 15:00:30 +0530

    push

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,42 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Cupcake {
-
-    public static long calcount(int n, int[] arr) {
-        long calories = 0;
-        long multiplier = 1;
-
-        for (int i = 0; i < n; i++) {
-            calories += arr[i] * multiplier;
-            multiplier *= 2;
-        }
-        return calories;
-    }
-
-    public static void bubblesort(int n, int[] arr) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] < arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int n = scan.nextInt();
-        int[] arr = new int[n];
+        int n = sc.nextInt();
+        long[] calories = new long[n];
 
         for (int i = 0; i < n; i++) {
-            arr[i] = scan.nextInt();
+            calories[i] = sc.nextLong();
         }
 
-        bubblesort(n, arr);
-        long result = calcount(n, arr);
-        System.out.println(result);
+        // Sort in descending order
+        Arrays.sort(calories);
+
+        long miles = 0;
+        for (int i = 0; i < n; i++) {
+            miles += calories[n - 1 - i] * (1L << i);
+        }
+
+        System.out.println(miles);
+        sc.close();
     }
 }
